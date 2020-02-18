e9c08307c192b63f57d5efd35aa8d0ab1223daef
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-02-17 21:24:11 -0500

    Update rainbow_hearts.R

--- rainbow_hearts.R
+++ rainbow_hearts.R
@@ -1,8 +1,13 @@
+#load packages
 library(ggplot2)
 library(egg)
 
+#make a heart function (a cardoid)
+#Refrence: https://math.stackexchange.com/questions/12098/drawing-heart-in-mathematica
 hearty <- function(x)2-2*sin(x) + sin(x)*(sqrt(abs(cos(x))))/(sin(x)+1.4)
 
+#feed in the 100 2*pi points
+#Then draw polar graphs by inversting the cardoids and applying different colors
 red <- ggplot()+ 
   stat_function(data = data.frame(x= seq(0, 2*pi, length.out= 200)), 
                 aes(x=x),fun= hearty,  geom= "area",fill = "red", alpha= 0.5) + 
@@ -33,7 +38,8 @@ pur <- ggplot()+
                 aes(x=x),fun= hearty,  geom= "area",fill = "purple", alpha= 0.5) + 
   coord_polar(start=-pi/2)+ theme_bw()
 
-
+#Join all the plots together in one row
 figures <- egg::ggarrange(red, ora, yel, gre, blu, pur, nrow = 1)
 
+#Save
 ggsave("hearts.png", figures, width = 25, height = 9, units = "cm", dpi= 600)
